7c92d2386a3726c70ec46b040966881329c672a4
Sandeep <sandeep@opstree.com>
2023-10-21 03:56:35 +0530

    Added checks for branch validation

--- build.sh
+++ build.sh
@@ -17,14 +17,28 @@ TASK_STATUS=0
 
 logInfoMessage "Merging branch ${SRC_BRANCH} to ${TGT_BRANCH}"
 
-CONFLICTING_FILES=`findConflictingFiles ${SRC_BRANCH} ${TGT_BRANCH}`
+branch_exists ${SRC_BRANCH}
+if [ "$?" -eq 0  ]; then
+    branch_exists ${TGT_BRANCH}
+    if [ "$?" -eq 0 ]; then
 
-if [ -z "${CONFLICTING_FILES}" ]; then
-    TASK_STATUS=0
-    logInfoMessage "${SRC_BRANCH} can be merged back into ${TGT_BRANCH}, please continue"
+        CONFLICTING_FILES=`findConflictingFiles ${SRC_BRANCH} ${TGT_BRANCH}`
+
+        if [ -z "${CONFLICTING_FILES}" ]; then
+            TASK_STATUS=0
+            logInfoMessage "${SRC_BRANCH} can be merged back into ${TGT_BRANCH}, please continue"
+        else
+            TASK_STATUS=1
+            logInfoMessage "Listing out the authors of conflicting files in source branch: ${SRC_BRANCH} vs target branch: ${TGT_BRANCH} "
+            listAuthorsOfFilesAcrossBranches ${SRC_BRANCH} ${TGT_BRANCH} "${CONFLICTING_FILES}"
+        fi
+    else
+        TASK_STATUS=1
+        logErrorMessage "${TGT_BRANCH} doesn't exists, please check!!!"
+    fi 
 else
     TASK_STATUS=1
-    logInfoMessage "Listing out the authors of conflicting files in source branch: ${SRC_BRANCH} vs target branch: ${TGT_BRANCH} "
-    listAuthorsOfFilesAcrossBranches ${SRC_BRANCH} ${TGT_BRANCH} "${CONFLICTING_FILES}"
-fi
+    logErrorMessage "${SRC_BRANCH} doesn't exists, please check!!!"
+fi 
+
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
